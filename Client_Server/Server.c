3e7c10d1d7c11d39308813d100a1155e2a7e8804
NagaSurya7585 <144314157+NagaSurya7585@users.noreply.github.com>
2023-09-12 11:32:04 +0530

    Update Server.c

--- Client_Server/Server.c
+++ Client_Server/Server.c
@@ -1,107 +1,72 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <winsock2.h> // Include Windows socket header
-#include <ws2tcpip.h> // Include Windows socket header
-#include <pthread.h>
+#include"Header.h"
+void client_info(struct sockaddr_in client_info)
+{
+printf("client port address:
+%d\n",client_info.sin_port);
+printf("client ip address:
+%s\n",inet_ntoa(client_info.sin_addr));
 
-#define PORT 8080
-#define MAX_CLIENTS 5
+}
+int main(int argc,char **argv)
+{
+if(argc!=3)
+{
+printf("usage: ./server server_port
 
-void* handleClient(void* clientSocket) {
-    SOCKET clientSock = *(SOCKET*)clientSocket;
-    char buffer[1024];
-    ssize_t bytesRead;
+server_ip_address\n");
 
-    while (1) {
-        // Receive data from the client
-        bytesRead = recv(clientSock, buffer, sizeof(buffer), 0);
-        if (bytesRead <= 0) {
-            perror("Error receiving data from client");
-            break;
-        }
+return 0;
+}
+int sfd,len,nsfd;
+struct sockaddr_in server_id,client_id;
+/********** socket () ***************/
+sfd=socket(AF_INET,SOCK_STREAM,0);
+if(sfd<0)
+{
+perror("Socket");
+return 0;
+}
+perror("Socket");
+/*********** bind() ***************/
+server_id.sin_family=AF_INET;
+server_id.sin_port=htons(atoi(argv[1]));
+server_id.sin_addr.s_addr=inet_addr(argv[2]);
 
-        // Process the received data (ping)
-        buffer[bytesRead] = '\0';
-        if (strcmp(buffer, "ping") == 0) {
-            // Send a response (pong)
-            send(clientSock, "pong", 4, 0);
-        }
-    }
+len=sizeof(server_id);
+if(bind(sfd,(struct sockaddr*)
+&server_id,len)<0)
+{
+perror("Bind");
+return 0;
+}
+perror("Bind");
 
-    // Close the client socket and exit the thread
-    closesocket(clientSock);
-    free(clientSocket);
-    pthread_exit(NULL);
+/************* listen **************/
+if(listen(sfd,5)<0)
+{
+perror("Listen");
+return 0;
 }
 
-int main() {
-    SOCKET serverSock, clientSock;
-    struct sockaddr_in serverAddr, clientAddr;
-    socklen_t addrLen = sizeof(clientAddr);
-    pthread_t threadID;
+perror("Listen");
+/************ accept ****************/
+printf("before accept client request...(waiting
+for client)\n");
+nsfd=accept(sfd,(struct
+sockaddr*)&client_id,&len);
+if(nsfd<0)
+{
+perror("Accept");
+return 0;
+}
+printf("client request Accepted...\n");
+client_info(client_id);
+char s[20];
+read(nsfd,s,sizeof(s)); // reading data from
+client
+printf("client data: %s\n",s);
 
-    // Initialize Winsock
-    WSADATA wsaData;
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        perror("Error initializing Winsock");
-        exit(EXIT_FAILURE);
-    }
+close(sfd);
+close(nsfd);
 
-    // Create socket
-    serverSock = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSock == INVALID_SOCKET) {
-        perror("Error creating socket");
-        WSACleanup(); // Cleanup Winsock
-        exit(EXIT_FAILURE);
-    }
-
-    // Set up server address
-    memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-    serverAddr.sin_port = htons(PORT);
-
-    // Bind the socket
-    if (bind(serverSock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
-        perror("Error binding socket");
-        closesocket(serverSock);
-        WSACleanup(); // Cleanup Winsock
-        exit(EXIT_FAILURE);
-    }
-
-    // Listen for incoming connections
-    if (listen(serverSock, MAX_CLIENTS) == SOCKET_ERROR) {
-        perror("Error listening for connections");
-        closesocket(serverSock);
-        WSACleanup(); // Cleanup Winsock
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server listening on port %d...\n", PORT);
-
-    while (1) {
-        // Accept incoming client connection
-        clientSock = accept(serverSock, (struct sockaddr*)&clientAddr, &addrLen);
-        if (clientSock == INVALID_SOCKET) {
-            perror("Error accepting client connection");
-            continue;
-        }
-
-        // Handle the client in a new thread
-        SOCKET* clientSockPtr = malloc(sizeof(SOCKET));
-        *clientSockPtr = clientSock;
-        if (pthread_create(&threadID, NULL, handleClient, clientSockPtr) != 0) {
-            perror("Error creating thread");
-            free(clientSockPtr);
-            closesocket(clientSock);
-            continue;
-        }
-    }
-
-    // Close the server socket (never reached in this example)
-    closesocket(serverSock);
-    WSACleanup(); // Cleanup Winsock
-
-    return 0;
 }
